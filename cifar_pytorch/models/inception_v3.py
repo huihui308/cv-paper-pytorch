5cd3707222ffc3999dd4c87d88a4cf8842e0832b
david <305573571@qq.com>
2023-07-31 14:26:44 +0800

    add cifar_pytorch/models/inception_v2.py

--- cifar_pytorch/models/inception_v3.py
+++ cifar_pytorch/models/inception_v3.py
@@ -11,133 +11,16 @@ __all__ = ["Inception3", "inception_v3"]
 _InceptionOuputs = namedtuple("InceptionOuputs", ["logits", "aux_logits"])
 
 
-def inception_v3(pretrained=False, progress=True, device="cpu", **kwargs):
-    r"""Inception v3 model architecture from
-    `"Rethinking the Inception Architecture for Computer Vision" <http://arxiv.org/abs/1512.00567>`_.
-
-    .. note::
-        **Important**: In contrast to the other models the inception_v3 expects tensors with a size of
-        N x 3 x 299 x 299, so ensure your images are sized accordingly.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-        aux_logits (bool): If True, add an auxiliary branch that can improve training.
-            Default: *True*
-        transform_input (bool): If True, preprocesses the input according to the method with which it
-            was trained on ImageNet. Default: *False*
-    """
-    model = Inception3()
-    if pretrained:
-        script_dir = os.path.dirname(__file__)
-        state_dict = torch.load(
-            script_dir + "/state_dicts/inception_v3.pt", map_location=device
-        )
-        model.load_state_dict(state_dict)
-    return model
-
-
-class Inception3(nn.Module):
-    # CIFAR10: aux_logits True->False
-    def __init__(self, num_classes=10, aux_logits=False, transform_input=False):
-        super(Inception3, self).__init__()
-        self.aux_logits = aux_logits
-        self.transform_input = transform_input
-
-        # CIFAR10: stride 2->1, padding 0 -> 1
-        self.Conv2d_1a_3x3 = BasicConv2d(3, 192, kernel_size=3, stride=1, padding=1)
-        #         self.Conv2d_2a_3x3 = BasicConv2d(32, 32, kernel_size=3)
-        #         self.Conv2d_2b_3x3 = BasicConv2d(32, 64, kernel_size=3, padding=1)
-        #         self.Conv2d_3b_1x1 = BasicConv2d(64, 80, kernel_size=1)
-        #         self.Conv2d_4a_3x3 = BasicConv2d(80, 192, kernel_size=3)
-        self.Mixed_5b = InceptionA(192, pool_features=32)
-        self.Mixed_5c = InceptionA(256, pool_features=64)
-        self.Mixed_5d = InceptionA(288, pool_features=64)
-        self.Mixed_6a = InceptionB(288)
-        self.Mixed_6b = InceptionC(768, channels_7x7=128)
-        self.Mixed_6c = InceptionC(768, channels_7x7=160)
-        self.Mixed_6d = InceptionC(768, channels_7x7=160)
-        self.Mixed_6e = InceptionC(768, channels_7x7=192)
-        if aux_logits:
-            self.AuxLogits = InceptionAux(768, num_classes)
-        self.Mixed_7a = InceptionD(768)
-        self.Mixed_7b = InceptionE(1280)
-        self.Mixed_7c = InceptionE(2048)
-        self.fc = nn.Linear(2048, num_classes)
-
-    #         for m in self.modules():
-    #             if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-    #                 import scipy.stats as stats
-    #                 stddev = m.stddev if hasattr(m, 'stddev') else 0.1
-    #                 X = stats.truncnorm(-2, 2, scale=stddev)
-    #                 values = torch.as_tensor(X.rvs(m.weight.numel()), dtype=m.weight.dtype)
-    #                 values = values.view(m.weight.size())
-    #                 with torch.no_grad():
-    #                     m.weight.copy_(values)
-    #             elif isinstance(m, nn.BatchNorm2d):
-    #                 nn.init.constant_(m.weight, 1)
-    #                 nn.init.constant_(m.bias, 0)
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
 
     def forward(self, x):
-        if self.transform_input:
-            x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
-            x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
-            x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
-            x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
-        # N x 3 x 299 x 299
-        x = self.Conv2d_1a_3x3(x)
-
-        # CIFAR10
-        # N x 32 x 149 x 149
-        #         x = self.Conv2d_2a_3x3(x)
-        # N x 32 x 147 x 147
-        #         x = self.Conv2d_2b_3x3(x)
-        # N x 64 x 147 x 147
-        #         x = F.max_pool2d(x, kernel_size=3, stride=2)
-        # N x 64 x 73 x 73
-        #         x = self.Conv2d_3b_1x1(x)
-        # N x 80 x 73 x 73
-        #         x = self.Conv2d_4a_3x3(x)
-        # N x 192 x 71 x 71
-        #         x = F.max_pool2d(x, kernel_size=3, stride=2)
-        # N x 192 x 35 x 35
-        x = self.Mixed_5b(x)
-        # N x 256 x 35 x 35
-        x = self.Mixed_5c(x)
-        # N x 288 x 35 x 35
-        x = self.Mixed_5d(x)
-        # N x 288 x 35 x 35
-        x = self.Mixed_6a(x)
-        # N x 768 x 17 x 17
-        x = self.Mixed_6b(x)
-        # N x 768 x 17 x 17
-        x = self.Mixed_6c(x)
-        # N x 768 x 17 x 17
-        x = self.Mixed_6d(x)
-        # N x 768 x 17 x 17
-        x = self.Mixed_6e(x)
-        # N x 768 x 17 x 17
-        if self.training and self.aux_logits:
-            aux = self.AuxLogits(x)
-        # N x 768 x 17 x 17
-        x = self.Mixed_7a(x)
-        # N x 1280 x 8 x 8
-        x = self.Mixed_7b(x)
-        # N x 2048 x 8 x 8
-        x = self.Mixed_7c(x)
-        # N x 2048 x 8 x 8
-        # Adaptive average pooling
-        x = F.adaptive_avg_pool2d(x, (1, 1))
-        # N x 2048 x 1 x 1
-        x = F.dropout(x, training=self.training)
-        # N x 2048 x 1 x 1
-        x = x.view(x.size(0), -1)
-        # N x 2048
-        x = self.fc(x)
-        # N x 1000 (num_classes)
-        if self.training and self.aux_logits:
-            return _InceptionOuputs(x, aux)
-        return x
+        x = self.conv(x)
+        x = self.bn(x)
+        return F.relu(x, inplace=True)
 
 
 class InceptionA(nn.Module):
@@ -324,13 +207,137 @@ class InceptionAux(nn.Module):
         return x
 
 
-class BasicConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, **kwargs):
-        super(BasicConv2d, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
-        self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
+class Inception3(nn.Module):
+    # CIFAR10: aux_logits True->False
+    def __init__(self, num_classes=10, aux_logits=False, transform_input=False):
+        super(Inception3, self).__init__()
+        self.aux_logits = aux_logits
+        self.transform_input = transform_input
+
+        # CIFAR10: stride 2->1, padding 0 -> 1
+        self.Conv2d_1a_3x3 = BasicConv2d(3, 192, kernel_size=3, stride=1, padding=1)
+        #         self.Conv2d_2a_3x3 = BasicConv2d(32, 32, kernel_size=3)
+        #         self.Conv2d_2b_3x3 = BasicConv2d(32, 64, kernel_size=3, padding=1)
+        #         self.Conv2d_3b_1x1 = BasicConv2d(64, 80, kernel_size=1)
+        #         self.Conv2d_4a_3x3 = BasicConv2d(80, 192, kernel_size=3)
+        self.Mixed_5b = InceptionA(192, pool_features=32)
+        self.Mixed_5c = InceptionA(256, pool_features=64)
+        self.Mixed_5d = InceptionA(288, pool_features=64)
+        self.Mixed_6a = InceptionB(288)
+        self.Mixed_6b = InceptionC(768, channels_7x7=128)
+        self.Mixed_6c = InceptionC(768, channels_7x7=160)
+        self.Mixed_6d = InceptionC(768, channels_7x7=160)
+        self.Mixed_6e = InceptionC(768, channels_7x7=192)
+        if aux_logits:
+            self.AuxLogits = InceptionAux(768, num_classes)
+        self.Mixed_7a = InceptionD(768)
+        self.Mixed_7b = InceptionE(1280)
+        self.Mixed_7c = InceptionE(2048)
+        self.fc = nn.Linear(2048, num_classes)
+
+    #         for m in self.modules():
+    #             if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+    #                 import scipy.stats as stats
+    #                 stddev = m.stddev if hasattr(m, 'stddev') else 0.1
+    #                 X = stats.truncnorm(-2, 2, scale=stddev)
+    #                 values = torch.as_tensor(X.rvs(m.weight.numel()), dtype=m.weight.dtype)
+    #                 values = values.view(m.weight.size())
+    #                 with torch.no_grad():
+    #                     m.weight.copy_(values)
+    #             elif isinstance(m, nn.BatchNorm2d):
+    #                 nn.init.constant_(m.weight, 1)
+    #                 nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        return F.relu(x, inplace=True)
+        if self.transform_input:
+            x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
+            x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
+            x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
+            x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
+        # N x 3 x 299 x 299
+        x = self.Conv2d_1a_3x3(x)
+
+        # CIFAR10
+        # N x 32 x 149 x 149
+        #         x = self.Conv2d_2a_3x3(x)
+        # N x 32 x 147 x 147
+        #         x = self.Conv2d_2b_3x3(x)
+        # N x 64 x 147 x 147
+        #         x = F.max_pool2d(x, kernel_size=3, stride=2)
+        # N x 64 x 73 x 73
+        #         x = self.Conv2d_3b_1x1(x)
+        # N x 80 x 73 x 73
+        #         x = self.Conv2d_4a_3x3(x)
+        # N x 192 x 71 x 71
+        #         x = F.max_pool2d(x, kernel_size=3, stride=2)
+        # N x 192 x 35 x 35
+        x = self.Mixed_5b(x)
+        # N x 256 x 35 x 35
+        x = self.Mixed_5c(x)
+        # N x 288 x 35 x 35
+        x = self.Mixed_5d(x)
+        # N x 288 x 35 x 35
+        x = self.Mixed_6a(x)
+        # N x 768 x 17 x 17
+        x = self.Mixed_6b(x)
+        # N x 768 x 17 x 17
+        x = self.Mixed_6c(x)
+        # N x 768 x 17 x 17
+        x = self.Mixed_6d(x)
+        # N x 768 x 17 x 17
+        x = self.Mixed_6e(x)
+        # N x 768 x 17 x 17
+        if self.training and self.aux_logits:
+            aux = self.AuxLogits(x)
+        # N x 768 x 17 x 17
+        x = self.Mixed_7a(x)
+        # N x 1280 x 8 x 8
+        x = self.Mixed_7b(x)
+        # N x 2048 x 8 x 8
+        x = self.Mixed_7c(x)
+        # N x 2048 x 8 x 8
+        # Adaptive average pooling
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        # N x 2048 x 1 x 1
+        x = F.dropout(x, training=self.training)
+        # N x 2048 x 1 x 1
+        x = x.view(x.size(0), -1)
+        # N x 2048
+        x = self.fc(x)
+        # N x 1000 (num_classes)
+        if self.training and self.aux_logits:
+            return _InceptionOuputs(x, aux)
+        return x
+
+
+def inception_v3(pretrained=False, progress=True, device="cpu", **kwargs):
+    r"""Inception v3 model architecture from
+    `"Rethinking the Inception Architecture for Computer Vision" <http://arxiv.org/abs/1512.00567>`_.
+
+    .. note::
+        **Important**: In contrast to the other models the inception_v3 expects tensors with a size of
+        N x 3 x 299 x 299, so ensure your images are sized accordingly.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+        aux_logits (bool): If True, add an auxiliary branch that can improve training.
+            Default: *True*
+        transform_input (bool): If True, preprocesses the input according to the method with which it
+            was trained on ImageNet. Default: *False*
+    """
+    model = Inception3()
+    if pretrained:
+        script_dir = os.path.dirname(__file__)
+        state_dict = torch.load(
+            script_dir + "/state_dicts/inception_v3.pt", map_location=device
+        )
+        model.load_state_dict(state_dict)
+    return model
+
+
+if __name__ == '__main__':
+    import argparse, torchsummary
+
+    model = inception_v3()
+    torchsummary.summary(model, input_size=(3, 32, 32), batch_size=1, device='cpu')
